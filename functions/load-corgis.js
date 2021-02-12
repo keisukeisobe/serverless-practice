4c3b797a6bad6d3c0cb9daa43c45224e30329048
keisukeisobe <52838946+keisukeisobe@users.noreply.github.com>
2021-02-12 17:48:18 -0500

    fixed fetch URLs

--- functions/load-corgis.js
+++ functions/load-corgis.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 
 exports.handler = async () => {
-  const corgis = await fetch('http://no-cors-api.netlify.app/api/corgis')
+  const corgis = await fetch('https://no-cors-api.netlify.app/api/corgis')
   .then((res) => res.json());
 
   const unsplashPromise = fetch(
@@ -9,21 +9,21 @@ exports.handler = async () => {
     {
       headers: {
         Authorization: `Client-ID ${process.env.UNSPLASH_ACCESS_KEY}`,
-      }
+      },
     }
   ).then((res) => res.json());
 
   const [unsplashData] = await Promise.all([unsplashPromise]);
 
   const completeData = corgis.map((corgi) => {
-    const photo = unsplashData.find(p => corgi.id === p.id);
+    const photo = unsplashData.find((p) => corgi.id === p.id);
     return {
       ...corgi,
       alt: photo.alt_description,
       credit: photo.user.name,
       url: `${photo.urls.raw}&auto=format&fit=crop&w=300&h=300&q=80&crop=entropy`,
-    }
-  })
+    };
+  });
 
   return {
     statusCode: '200',
@@ -31,5 +31,5 @@ exports.handler = async () => {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify(completeData)
-  }
-}
+  };
+};
