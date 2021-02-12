bbb29673198b8ccdeb83e73df33c36107dd45767
keisukeisobe <52838946+keisukeisobe@users.noreply.github.com>
2021-02-11 21:07:43 -0500

    added serverless function

---
+++ functions/load-corgis.js
@@ -0,0 +1,13 @@
+const fetch = require('node-fetch');
+
+exports.handler = async () => {
+  const corgis = await fetch('http://no-cors-api.netlify.app/api/corgis')
+  .then((res) => res.json());
+  return {
+    statusCode: '200',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(corgis)
+  }
+}
