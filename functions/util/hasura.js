f253b46c61c48e40e37b55f6cf120162a7278f83
keisukeisobe <52838946+keisukeisobe@users.noreply.github.com>
2021-02-15 20:55:41 -0500

    booping update and creating added

---
+++ functions/util/hasura.js
@@ -0,0 +1,17 @@
+const fetch = require('node-fetch');
+
+exports.hasuraRequest = async ({query, variables}) => {
+  const result = await fetch(process.env.HASURA_URL, {
+    method: 'POST',
+    headers: {
+      'X-Hasura-Admin-Secret': process.env.HASURA_ADMIN_SECRET,
+    },
+    body: JSON.stringify({query, variables}),
+  }).then((res) => res.json());
+
+  if (!result || !result.data) {
+    console.error(result);
+    return [];
+  };
+  return result.data; 
+};
